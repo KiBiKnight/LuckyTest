5de3ccd6f6b87f4001da29e8778720d0259e1ccd
Casparus Bresler <casparusb@agilebridge.co.za>
2026-02-18 14:27:14 +0200

    Serialize enum

--- LuckyClean/LuckyCLean.API/Program.cs
+++ LuckyClean/LuckyCLean.API/Program.cs
@@ -1,12 +1,17 @@
 using LuckyClean.Application;
 using LuckyClean.Infrastructure;
 using LuckyCLean.API.Middleware;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
